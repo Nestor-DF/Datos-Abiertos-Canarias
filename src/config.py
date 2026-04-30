484b164478798a69ca91be51d9cea7a9a478ef2f
Steven R <stevenr@MacBook-Air-Steven-10.local>
2026-04-30 15:46:11 +0100

    Actualizados los datos del ISTAC

--- src/config.py
+++ src/config.py
@@ -20,58 +20,58 @@ MAX_RECORDS_DOWNLOAD = int(os.getenv("MAX_RECORDS_DOWNLOAD", "1000"))
 
 #Fuentes Hardcoded Iniciales
 SOURCES = [
-    # {
-    #     "id": "gobcan",
-    #     "name": "Gobierno de Canarias",
-    #     "url": "https://datos.canarias.es/catalogos/general",
-    #     "type": "Especializado"
-    # },
-    # {
-    #     "id": "sitcan",
-    #     "name": "SITCAN",
-    #     "url": "https://opendata.sitcan.es",
-    #     "type": "Especializado"
-    # },
-    # {
-    #     "id": "istac",
-    #     "name": "ISTAC",
-    #     "url": "https://datos.canarias.es/catalogos/estadisticas",
-    #     "type": "Especializado"
-    # }
-    # {
-    #     "id": "tenerife",
-    #     "name": "Cabildo de Tenerife",
-    #     "url": "https://datos.tenerife.es/ckan",
-    #     "type": "Cabildo"
-    # },
-    #{
-    #     "id": "lapalma",
-    #     "name": "Cabildo de La Palma",
-    #     "url": "https://lapalmasmart-open.lapalma.es/datosabiertos/catalogo",
-    #     "type": "Cabildo"
-    # },
-    # {
-    #     "id": "elhierro",
-    #     "name": "Cabildo de El Hierro",
-    #     "url": "https://datosabiertos.elhierro.es",
-    #     "type": "Cabildo"
-    # },
-    # {
-    #     "id": "fuerteventura",
-    #     "name": "Cabildo de Fuerteventura",
-    #     "url": "https://gobiernoabierto.cabildofuer.es/datosabiertos/catalogo",
-    #     "type": "Cabildo"
-    # },
-    # {
-    #     "id": "lpgc",
-    #     "name": "Ayuntamiento de Las Palmas de Gran Canaria",
-    #     "url": "http://apidatosabiertos.laspalmasgc.es",
-    #     "type": "Ayuntamiento"
-    # }, 
-    # {
-    #     "id": "parcan",
-    #     "name": "Parlamento de Canarias",
-    #     "url": "https://datos.parcan.es",
-    #     "type": "Especializado"
-    # },
+    {
+        "id": "gobcan",
+        "name": "Gobierno de Canarias",
+        "url": "https://datos.canarias.es/catalogos/general",
+        "type": "Especializado"
+    },
+    {
+        "id": "sitcan",
+        "name": "SITCAN",
+        "url": "https://opendata.sitcan.es",
+        "type": "Especializado"
+    },
+    {
+       "id": "istac",
+       "name": "ISTAC",
+       "url": "https://datos.canarias.es/catalogos/estadisticas",
+       "type": "Especializado"
+    },
+    {
+        "id": "tenerife",
+        "name": "Cabildo de Tenerife",
+        "url": "https://datos.tenerife.es/ckan",
+        "type": "Cabildo"
+    },
+    {
+        "id": "lapalma",
+        "name": "Cabildo de La Palma",
+        "url": "https://lapalmasmart-open.lapalma.es/datosabiertos/catalogo",
+        "type": "Cabildo"
+    },
+    {
+        "id": "elhierro",
+        "name": "Cabildo de El Hierro",
+        "url": "https://datosabiertos.elhierro.es",
+        "type": "Cabildo"
+    },
+    {
+        "id": "fuerteventura",
+        "name": "Cabildo de Fuerteventura",
+        "url": "https://gobiernoabierto.cabildofuer.es/datosabiertos/catalogo",
+        "type": "Cabildo"
+    },
+    {
+        "id": "lpgc",
+        "name": "Ayuntamiento de Las Palmas de Gran Canaria",
+        "url": "http://apidatosabiertos.laspalmasgc.es",
+        "type": "Ayuntamiento"
+    }, 
+    {
+        "id": "parcan",
+        "name": "Parlamento de Canarias",
+        "url": "https://datos.parcan.es",
+        "type": "Especializado"
+    },
 ]
